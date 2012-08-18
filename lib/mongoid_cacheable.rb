bf0e83547a6164de848632b49f4013b4399fb761
Noah H. Smith <nsmith@rendergardenmedia.com>
2012-08-18 11:37:15 -0500

    redefined cache interface

--- lib/mongoid_cacheable.rb
+++ lib/mongoid_cacheable.rb
@@ -9,32 +9,28 @@ module Mongoid
     module ClassMethods
       def cache( name, *options )
         field_name = "_#{name}"
-        uncached_name = "uncached_#{name}"
         cached_name = "cached_#{name}"
         clear_cached_name = "clear_cached_#{name}"
 
         field field_name, *options
 
-        alias_method uncached_name, name
-        alias_method cached_name, field_name
-
-        define_method(name) do
-          cache_field(field_name, &method(uncached_name))
+        define_method(cached_name) do
+          cache_field(field_name, &method(name))
         end
 
         define_method(clear_cached_name) do
-          cache_field(field_name)
+          clear_cache_field(field_name)
         end
       end
     end
 
     def cache_field(field_name, &block)
-      unless attributes[field_name]
+      unless read_attribute(field_name)
         # cache quitely with atomic set
         set field_name, yield
       end
 
-      attributes[field_name]
+      read_attribute(field_name)
     end
 
     def clear_cache_field(field_name)
